e7e3ee433c0766ae90b0b7cd9870e77bb7147ff6
zsabyrzhanov <zamirsabyrzhanov@gmail.com>
2022-06-13 17:54:51 +0600

    transaction done

--- src/main/java/com/peaksoft/accounting/validation/exception/ValidationExceptionType.java
+++ src/main/java/com/peaksoft/accounting/validation/exception/ValidationExceptionType.java
@@ -7,15 +7,16 @@ public class ValidationExceptionType {
     public static final String PASSWORDS_DONT_MATCH = "PASSWORDS_DONT_MATCH";
     public static final String LOGIN_FAILED = "Username or password is incorrect!";
     public static final String SUCCESSFUL = "Successful!";
-    public static final String COMPANY_ALREADY_EXISTS = "COMPANY_ALREADY_EXISTS";
-    public static final String TAG_ALREADY_EXISTS = "TAG_ALREADY_EXISTS";
-    public static final String CLIENT_NOT_FOUND = "ClIENT_NOT_FOUND";
-    public static final String TAG_NOT_FOUND = "TAG_NOT_FOUND";
-    public static final String BANK_ACCOUNT_NOT_FOUND = "BANK_ACCOUNT_NOT_FOUND";
-    public static final String PRODUCT_NOT_FOUND = "PRODUCT_NOT_FOUND";
-    public static final String INVOICE_NOT_FOUND = "INVOICE_NOT_FOUND";
-    public static final String NOT_FOUND = "NOT_FOUND";
-    public static final String THIS_NAME_ALREADY_EXISTS = "THIS_NAME_ALREADY_EXISTS";
-    public static final String EXCEEDS_THE_AMOUNT_PER_PRODUCT = "EXCEEDS_THE_AMOUNT_PER_PRODUCT";
+    public static final String COMPANY_ALREADY_EXISTS = "COMPANY ALREADY EXISTS";
+    public static final String TAG_ALREADY_EXISTS = "TAG ALREADY EXISTS";
+    public static final String CLIENT_NOT_FOUND = "ClIENT NOT FOUND";
+    public static final String TAG_NOT_FOUND = "TAG NOT FOUND";
+    public static final String BANK_ACCOUNT_NOT_FOUND = "BANK ACCOUNT NOT FOUND";
+    public static final String PRODUCT_NOT_FOUND = "PRODUCT NOT FOUND";
+    public static final String INVOICE_NOT_FOUND = "INVOICE NOT FOUND";
+    public static final String NOT_FOUND = "NOT FOUND";
+    public static final String THIS_NAME_ALREADY_EXISTS = "THIS NAME ALREADY_EXISTS";
+    public static final String EXCEEDS_THE_AMOUNT_PER_PRODUCT = "EXCEEDS THE AMOUNT PER PRODUCT";
+    public static final String PAY_THE_FULL_AMOUNT = "PAY THE FULL AMOUNT";
 
 }
