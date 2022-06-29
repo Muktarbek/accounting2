2945c3abd5057283fac9040a9a413e238f8e603e
apple <muktarbeks0101@gmail.com>
2022-06-29 05:34:02 -0700

    create user

--- src/main/java/com/peaksoft/accounting/AccountingApplication.java
+++ src/main/java/com/peaksoft/accounting/AccountingApplication.java
@@ -19,6 +19,7 @@ public class AccountingApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountingApplication.class, args);
     }
+
     //@PostConstruct
     private void saveRole(){
         RoleEntity role = new RoleEntity();
